ae3d8dec9d51771ba4f02cafbf816992a96e6b47
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-04-25 23:50:59 +0530

    redux store setup

--- tech-pad-frontend/src/main.jsx
+++ tech-pad-frontend/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import { store } from "./app/store.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 );

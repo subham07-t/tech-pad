44c215884e834513b9525ebd9ae61334ec4b9a63
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-06-04 22:10:09 +0530

    login functionality added with token

--- tech-pad-frontend/src/components/DashHeader.jsx
+++ tech-pad-frontend/src/components/DashHeader.jsx
@@ -1,17 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate, Link, useLocation } from "react-router-dom";
+
+import { useSendLogoutMutation } from "../features/auth/authApiSlice";
+
+const DASH_REGEX = /^\/dash(\/)?$/;
+const NOTES_REGEX = /^\/dash\/notes(\/)?$/;
+const USERS_REGEX = /^\/dash\/users(\/)?$/;
 
 const DashHeader = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const [sendLogout, { isLoading, isSuccess, isError, error }] =
+    useSendLogoutMutation();
+
+  useEffect(() => {
+    if (isSuccess) navigate("/");
+  }, [isSuccess, navigate]);
+
+  if (isLoading) return <p>Logging Out...</p>;
+
+  if (isError) return <p>Error: {error.data?.message}</p>;
+
+  let dashClass = null;
+  if (
+    !DASH_REGEX.test(pathname) &&
+    !NOTES_REGEX.test(pathname) &&
+    !USERS_REGEX.test(pathname)
+  ) {
+    dashClass = "dash-header__container--small";
+  }
+
+  const logoutButton = (
+    <button className="icon-button" title="Logout" onClick={sendLogout}>
+      <FontAwesomeIcon icon={faRightFromBracket} />
+    </button>
+  );
+
   return (
-    <header className="dashboard-header">
-      <div className="dashboard-header-container">
+    <header className="dash-header">
+      <div className={`dash-header__container ${dashClass}`}>
         <Link to="/dashboard">
-          <h1 className="dashboard-header-title">techNotes</h1>
+          <h1 className="dash-header__title">techNotes</h1>
         </Link>
-        <nav className="dashboard-header-nav"></nav>
+        <nav className="dash-header__nav">
+          {/* add more buttons later */}
+          {logoutButton}
+        </nav>
       </div>
     </header>
   );
 };
-
 export default DashHeader;
--- tech-pad-frontend/src/features/notes/NewNote.jsx
+++ tech-pad-frontend/src/features/notes/NewNote.jsx
@@ -4,8 +4,9 @@ import NewNoteForm from "./NewNoteForm";
 
 const NewNote = () => {
   const users = useSelector(selectAllUsers);
-  const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>;
 
-  return content;
+  if (!users?.length) return <p>Not Currently Available</p>;
+
+  return <NewNoteForm users={users} />;
 };
 export default NewNote;
